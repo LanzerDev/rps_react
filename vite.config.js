b07dd3e924d556317d83401416a3683748d87131
Lanzer <hdsnpej@gmail.com>
2022-03-18 19:37:32 -0700

    dist pls work

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/LanzerDev/rps-react.git',
+  base: '/rps_react/',
   plugins: [react(), ViteAliases([
     {
       find: '@assets',
